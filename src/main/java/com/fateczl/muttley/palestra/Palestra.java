a08a7552e49b8662f929306214598e30af6990db
Larissa Muller <lariitcmuller@gmail.com>
2026-03-22 00:45:00 -0300

    Adding Palestra.java

---
+++ src/main/java/com/fateczl/muttley/palestra/Palestra.java
@@ -0,0 +1,37 @@
+package com.fateczl.muttley.palestra;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="palestra")
+@Getter
+@Setter 
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of ="id")
+
+public class Palestra {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    //private List<Competencia> competencias;
+    //private List<Palestrante> palestrantes;
+    private LocalDateTime inicio;
+    private LocalDateTime fim;
+}
+
