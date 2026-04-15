eb106916f325006a8535e311e1c74973c8e080bb
AllexTay <allextaylerr@gmail.com>
2026-04-15 09:42:38 -0300

    Service, Mapper e construtores

--- src/main/java/com/fateczl/muttley/competencia/CompetenciaMapper.java
+++ src/main/java/com/fateczl/muttley/competencia/CompetenciaMapper.java
@@ -1,13 +1,23 @@
 package com.fateczl.muttley.competencia;
 
 import org.mapstruct.Mapper;
-//import org.mapstruct.Mapping;
-//import org.mapstruct.MappingTarget;
-//import org.mapstruct.Named;
-
-
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
-public class CompetenciaMapper {
+public interface CompetenciaMapper {
     
+    // Converte Entity para DTO (para preencher formulário de edição)
+    CompetenciaDTO toCompetenciaDTO (Competencia competencia);
+    
+    // Converte DTO para Entity (para criação NOVA - ignora ID)
+    @Mapping(target = "id", ignore = true)
+    Competencia toEntityFromDTO (CompetenciaDTO dto);
+
+    // Atualiza Entity existente com dados do DTO
+    @Mapping(target = "id", ignore = true)
+    void updateEntityFromDto(CompetenciaDTO dto, @MappingTarget Competencia competencia);
+
+
+
 }
